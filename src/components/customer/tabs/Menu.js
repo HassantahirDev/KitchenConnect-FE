5395a059dc3b03600288c22b3872be40af36c334
hassantahir7 <hassantahir3556@gmail.com>
2024-08-22 14:12:01 +0500

    added sidebar

--- src/components/customer/tabs/Menu.js
+++ src/components/customer/tabs/Menu.js
@@ -39,7 +39,7 @@ const menuItems = [
     price: "$15",
     image:
       "https://images.unsplash.com/photo-1516685018646-549198525c1b?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
+  }, 
   {
     day: 4,
     name: "Caesar Salad",
